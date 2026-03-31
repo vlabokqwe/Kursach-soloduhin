38ab6ea19cc0896692f6c98cf189f4d482925741
fhf <sjjjjaja@gmail.com>
2026-03-31 10:07:27 +0300

    remove trash

--- Kursach soloduhin/App.xaml.cs
+++ Kursach soloduhin/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace Kursach_soloduhin
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
